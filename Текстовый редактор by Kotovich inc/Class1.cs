778d648587b203f85569254c3cc3310aaf31ec56
KotovichTape <isip_d.d.kotovich@mpt.ru>
2022-12-27 23:30:43 +0300

    Добавьте файлы проекта.

---
+++ Текстовый редактор by Kotovich inc/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text.Json;
+
+namespace Text_converter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            converter.txtconverter();
+        }
+    }
+}
